b8fd444fe33b1038cf0cd0241714115c5d8565a6
rvndrupal <rodrigodrupal1@gmail.com>
2019-02-18 13:16:16 -0600

    Cambios variable nombre

--- modules/custom/panel/src/Controller/PanelController.php
+++ modules/custom/panel/src/Controller/PanelController.php
@@ -3,7 +3,10 @@
 
 namespace Drupal\panel\Controller;
 
+use Drupal\user\Entity\User;
+
 use Drupal\Core\Controller\ControllerBase;
+
 
 /**
  * An example controller.
@@ -13,12 +16,30 @@ class PanelController extends ControllerBase {
   /**
    * Returns a render-able array for a test page.
    */
+  
+  /*	$user = User::load(Drupal::currentUser()->id());
+	//$nom = $user->get('name')->value; 
+	//$imgUsuario = $user->get('user_picture')->target_id; 
+
+	dpm($user);*/
+	 //$nom = "rodrigo";
+
+
+
   public function index() {
+
+	 
+	
+
+     $user = \Drupal\user\Entity\User::load(\Drupal::currentUser()->id());
+     ksm($user);
+     $nombre=$user->get('name')->value;
+     dpm($nombre);
 
   	return[
 
   		'#theme' => 'panel_control',
-		//'#title' => 'Rodrigo Villanueva Nieto'
+		'#nombre' => $nombre,
 	];
   }
 }
